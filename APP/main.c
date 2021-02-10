379fcbfba574dfc48cef25c79808d5c27a519e22
Ahmed Kishk <ahmedadelkishk@gmail.com>
2021-02-10 04:20:03 +0200

    Documenting the code

--- APP/main.c
+++ APP/main.c
@@ -25,27 +25,27 @@
 /*	Function Prototype	*/
 
 int main(void) {
-	CLCD_voidInit();											//INITIALIZE LCD
-	CLCD_voidSendExtraChar();	//STORING SPECIAL CHARACTER (ARABIC) TO CGRAM
+	CLCD_voidInit();														//INITIALIZE LCD
+	CLCD_voidSendExtraChar();												//STORING SPECIAL CHARACTER (ARABIC) TO CGRAM
 	u8 LOC_u8Iterator = 0;
 	while (1) {
-		CLCD_voidSetPosition(CLCD_ROW_1, CLCD_COL_2);	//INITIALIZE CURSOR POSTION
+		CLCD_voidSetPosition(CLCD_ROW_1, CLCD_COL_2);						//SET CURSOR POSITION
 
-		CLCD_voidSendString((u8 *) "MO-SALAH STORY");
+		CLCD_voidSendString((u8 *) "MO-SALAH STORY");						//PRINT WORD IN LCD
 
 		for (LOC_u8Iterator = ZERO; LOC_u8Iterator < 8; LOC_u8Iterator++) {
-			CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_9);//INITIALIZE CURSOR POSTION
-			CLCD_voidSendData(RIGHT_RUNNING_PLAYER);
-			CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_7);//INITIALIZE CURSOR POSTION
-			CLCD_voidSendData(LEFT_RUNNING_PLAYER);
-			CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_8);//INITIALIZE CURSOR POSTION
-			CLCD_voidSendData(BALL_DOWN);
+			CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_9);					//SET CURSOR POSITION
+			CLCD_voidSendData(RIGHT_RUNNING_PLAYER);						//PRINT SPECIAL CHARACTER IN LCD
+			CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_7);					//SET CURSOR POSITION
+			CLCD_voidSendData(LEFT_RUNNING_PLAYER);							//PRINT SPECIAL CHARACTER IN LCD
+			CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_8);					//SET CURSOR POSITION
+			CLCD_voidSendData(BALL_DOWN);									//PRINT SPECIAL CHARACTER IN LCD
 			_delay_ms(300);
-			CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_9);//INITIALIZE CURSOR POSTION
-			CLCD_voidSendData(LEFT_RUNNING_PLAYER);
-			CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_7);//INITIALIZE CURSOR POSTION
-			CLCD_voidSendData(RIGHT_RUNNING_PLAYER);
-			CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_8);//INITIALIZE CURSOR POSTION
+			CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_9);					//SET CURSOR POSITION
+			CLCD_voidSendData(LEFT_RUNNING_PLAYER);							//PRINT SPECIAL CHARACTER IN LCD
+			CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_7);					//SET CURSOR POSITION
+			CLCD_voidSendData(RIGHT_RUNNING_PLAYER);						//PRINT SPECIAL CHARACTER IN LCD
+			CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_8);					//SET CURSOR POSITION
 			CLCD_voidSendData(BALL_UP);
 			_delay_ms(300);
 
@@ -53,86 +53,85 @@ int main(void) {
 
 		CLCD_voidClearDisplay();
 
-		CLCD_voidSetPosition(CLCD_ROW_1, CLCD_COL_1);//INITIALIZE CURSOR POSTION
-		CLCD_voidSendString((u8 *) "PLAYER HAS BALL");
+		CLCD_voidSetPosition(CLCD_ROW_1, CLCD_COL_1);						//SET CURSOR POSITION
+		CLCD_voidSendString((u8 *) "PLAYER HAS BALL");						//PRINT WORD IN LCD
 
 		for (LOC_u8Iterator = 0; LOC_u8Iterator < 5; LOC_u8Iterator++) {
-			CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_3 + LOC_u8Iterator);//INITIALIZE CURSOR POSTION
-			CLCD_voidSendData(RIGHT_RUNNING_PLAYER);
-			CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_4 + LOC_u8Iterator);//INITIALIZE CURSOR POSTION
-			CLCD_voidSendData(BALL_DOWN);
-			CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_13 - LOC_u8Iterator);	//INITIALIZE CURSOR POSTION
-			CLCD_voidSendData(LEFT_RUNNING_PLAYER);
+			CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_3 + LOC_u8Iterator);  //SET CURSOR POSITION
+			CLCD_voidSendData(RIGHT_RUNNING_PLAYER);						//PRINT SPECIAL CHARACTER IN LCD
+			CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_4 + LOC_u8Iterator);  //SET CURSOR POSITION
+			CLCD_voidSendData(BALL_DOWN);									//PRINT SPECIAL CHARACTER IN LCD
+			CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_13 - LOC_u8Iterator); //SET CURSOR POSITION
+			CLCD_voidSendData(LEFT_RUNNING_PLAYER);							//PRINT SPECIAL CHARACTER IN LCD
 			_delay_ms(250);
-			CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_3 + LOC_u8Iterator);//INITIALIZE CURSOR POSTION
-			CLCD_voidSendString((u8 *) " ");
-			CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_4 + LOC_u8Iterator);//INITIALIZE CURSOR POSTION
-			CLCD_voidSendString((u8 *) " ");
-			CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_13 - LOC_u8Iterator);	//INITIALIZE CURSOR POSTION
-			CLCD_voidSendString((u8 *) " ");
+			CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_3 + LOC_u8Iterator);  //SET CURSOR POSITION
+			CLCD_voidSendString((u8 *) " ");								//DELETE PREVIOUS CHARACTER
+			CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_4 + LOC_u8Iterator);  //SET CURSOR POSITION
+			CLCD_voidSendString((u8 *) " ");								//DELETE PREVIOUS CHARACTER
+			CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_13 - LOC_u8Iterator); //SET CURSOR POSITION
+			CLCD_voidSendString((u8 *) " ");								//DELETE PREVIOUS CHARACTER
 		}
 
-		CLCD_voidSetPosition(CLCD_ROW_1, CLCD_COL_1);//INITIALIZE CURSOR POSTION
-		CLCD_voidSendString((u8 *) "MoSalah GET BALL");
-		CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_8);//INITIALIZE CURSOR POSTION
-		CLCD_voidSendData(LEFT_RUNNING_PLAYER);
-		CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_7);//INITIALIZE CURSOR POSTION
-		CLCD_voidSendData(BALL_UP);
-		CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_9);//INITIALIZE CURSOR POSTION
-		CLCD_voidSendData(RIGHT_RUNNING_PLAYER);
+		CLCD_voidSetPosition(CLCD_ROW_1, CLCD_COL_1);						//SET CURSOR POSITION
+		CLCD_voidSendString((u8 *) "MoSalah GET BALL");						//PRINT WORD IN LCD
+		CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_8);						//SET CURSOR POSITION
+		CLCD_voidSendData(LEFT_RUNNING_PLAYER);								//PRINT SPECIAL CHARACTER IN LCD
+		CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_7);						//SET CURSOR POSITION
+		CLCD_voidSendData(BALL_UP);											//PRINT SPECIAL CHARACTER IN LCD
+		CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_9);						//SET CURSOR POSITION
+		CLCD_voidSendData(RIGHT_RUNNING_PLAYER);							//PRINT SPECIAL CHARACTER IN LCD
 
-		_delay_ms(1000);
-		CLCD_voidClearDisplay();
-		CLCD_voidSetPosition(CLCD_ROW_1, CLCD_COL_1);//INITIALIZE CURSOR POSTION
-		CLCD_voidSendString((u8 *) "SALAH IS RUNNING");
-		CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_7);//INITIALIZE CURSOR POSTION
-		CLCD_voidSendData(LEFT_RUNNING_PLAYER);
-		CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_6);//INITIALIZE CURSOR POSTION
-		CLCD_voidSendData(BALL_DOWN);
-		CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_9);//INITIALIZE CURSOR POSTION
-		CLCD_voidSendData(RIGHT_RUNNING_PLAYER);
+		_delay_ms(1000);													//DELAY FOR ONE SECOND
+		CLCD_voidClearDisplay();											//CLEAR LCD DISPLAY
+		CLCD_voidSetPosition(CLCD_ROW_1, CLCD_COL_1);						//SET CURSOR POSITION
+		CLCD_voidSendString((u8 *) "SALAH IS RUNNING");						//PRINT WORD IN LCD
+		CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_7);						//SET CURSOR POSITION
+		CLCD_voidSendData(LEFT_RUNNING_PLAYER);								//PRINT SPECIAL CHARACTER IN LCD
+		CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_6);						//SET CURSOR POSITION
+		CLCD_voidSendData(BALL_DOWN);										//PRINT SPECIAL CHARACTER IN LCD
+		CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_9);						//SET CURSOR POSITION
+		CLCD_voidSendData(RIGHT_RUNNING_PLAYER);							//PRINT SPECIAL CHARACTER IN LCD
 
-		_delay_ms(1000);
-		CLCD_voidClearDisplay();
-		CLCD_voidSetPosition(CLCD_ROW_1, CLCD_COL_3);//INITIALIZE CURSOR POSTION
-		CLCD_voidSendString((u8 *) "SALAH SHOOT!");
-		CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_7);//INITIALIZE CURSOR POSTION
-		CLCD_voidSendData(SHOOT_PLAYER);
-		CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_6);//INITIALIZE CURSOR POSTION
-		CLCD_voidSendData(BALL_DOWN);
-		CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_9);//INITIALIZE CURSOR POSTION
-		CLCD_voidSendData(RIGHT_RUNNING_PLAYER);
-		_delay_ms(200);
+		_delay_ms(1000);													//DELAY FOR ONE SECOND
+		CLCD_voidClearDisplay();											//CLEAR LCD DISPLAY
+		CLCD_voidSetPosition(CLCD_ROW_1, CLCD_COL_3);						//SET CURSOR POSITION
+		CLCD_voidSendString((u8 *) "SALAH SHOOT!");							//PRINT WORD IN LCD
+		CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_7);						//SET CURSOR POSITION
+		CLCD_voidSendData(SHOOT_PLAYER);									//PRINT SPECIAL CHARACTER IN LCD
+		CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_6);						//SET CURSOR POSITION
+		CLCD_voidSendData(BALL_DOWN);										//PRINT SPECIAL CHARACTER IN LCD
+		CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_9);						//SET CURSOR POSITION
+		CLCD_voidSendData(RIGHT_RUNNING_PLAYER);							//PRINT SPECIAL CHARACTER IN LCD
+		_delay_ms(200);														//DELAY FOR 200 MILLISECONDS
 
-		CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_6);//INITIALIZE CURSOR POSTION
-		CLCD_voidSendString((u8 *) " ");
+		CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_6);						//SET CURSOR POSITION
+		CLCD_voidSendString((u8 *) " ");									//PRINT FREE SPACE TO DELETE PREVIOUS CHARACTER
 
 		for (LOC_u8Iterator = 1; LOC_u8Iterator <= 6; LOC_u8Iterator++) {
 
-			CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_6);//INITIALIZE CURSOR POSTION
-			CLCD_voidSendData(BALL_UP);
-			CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_6 - LOC_u8Iterator);//INITIALIZE CURSOR POSTION
-			CLCD_voidSendData(BALL_UP);
+			CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_6);					//SET CURSOR POSITION
+			CLCD_voidSendData(BALL_UP);										//PRINT SPECIAL CHARACTER IN LCD
+			CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_6 - LOC_u8Iterator);	//SET CURSOR POSITION
+			CLCD_voidSendData(BALL_UP);										//PRINT SPECIAL CHARACTER IN LCD
 
 			_delay_ms(200);
 		}
 		CLCD_voidClearDisplay();
 		for (LOC_u8Iterator = 1; LOC_u8Iterator <= 6; LOC_u8Iterator++) {
-			CLCD_voidSetPosition(CLCD_ROW_1, CLCD_COL_3);//INITIALIZE CURSOR POSTION
-			CLCD_voidSendString((u8 *) "GOALLLL!!!");
-			CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_8);//INITIALIZE CURSOR POSTION
-			CLCD_voidSendData(RIGHT_RUNNING_PLAYER);
-			_delay_ms(500);
-			CLCD_voidSetPosition(CLCD_ROW_1, CLCD_COL_3);//INITIALIZE CURSOR POSTION
-			CLCD_voidSendString((u8 *) "          ");
-			CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_8);//INITIALIZE CURSOR POSTION
-			CLCD_voidSendData(LEFT_RUNNING_PLAYER);
-			_delay_ms(500);
+			CLCD_voidSetPosition(CLCD_ROW_1, CLCD_COL_3);					//SET CURSOR POSITION
+			CLCD_voidSendString((u8 *) "GOALLLL!!!");						//PRINT WORD IN LCD
+			CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_8);					//SET CURSOR POSITION
+			CLCD_voidSendData(RIGHT_RUNNING_PLAYER);						//PRINT SPECIAL CHARACTER IN LCD
+			_delay_ms(500);													//DELAY FOR HALF SECOND
+			CLCD_voidSetPosition(CLCD_ROW_1, CLCD_COL_3);					//SET CURSOR POSITION
+			CLCD_voidSendString((u8 *) "          ");						//DELETE ALL OVER THE WORD IN FIRST ROW
+			CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_8);					//SET CURSOR POSITION
+			CLCD_voidSendData(LEFT_RUNNING_PLAYER);							//PRINT SPECIAL CHARACTER IN LCD
+			_delay_ms(500);													//DELAY FOR HALF SECOND
 
 		}
 
 	}
 }
 
-/*	FUNCTION PROTOTYPE	*/
 
